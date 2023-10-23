e6b01843f0770909445f6bbb28e47be87e5f5685
John Trujano <john51.jtg@gmail.com>
2023-10-23 17:32:35 -0600

    Pagina mycleth Actualizacion 3

--- resources/views/planes.blade.php
+++ resources/views/planes.blade.php
@@ -3,76 +3,126 @@
 @section('content')
 PLANES
 <section class="section section-sm section-bottom-70 section-fluid bg-default">
-        <div class="container">
-          <h2>Pricing</h2>
-          <div class="row row-30 justify-content-center">
-            <div class="col-md-6 col-lg-5 col-xl-4">
-              <div class="box-pricing box-pricing-black">
-                <div class="box-pricing-body">
-                  <h5 class="box-pricing-title">basic</h5>
-                  <h3 class="box-pricing-price">$500.00</h3>
-                  <div class="box-pricing-time">starting at</div>
-                  <div class="box-pricing-divider">
-                    <div class="divider"></div><span>Basic</span>
-                  </div>
-                  <ul class="box-pricing-list">
-                    <li class="active">Concept development</li>
-                    <li class="active">UI design</li>
-                    <li>Configuration management</li>
-                    <li>Software quality assurance</li>
-                    <li>App integration</li>
-                  </ul>
+    <div class="container">
+        <h2>Planes</h2>
+        <div class="row row-30 justify-content-center">
+            <div class="col-md-6 col-lg-5 col-xl-4" >
+                <div class="box-pricing box-pricing-black">
+                    <div class="box-pricing-body">
+                        <h5 class="box-pricing-title">PERSONAL <br>PÁGINA CLÁSICA</h5>
+                        <div class="divider"></div>
+                        <h3 class="box-pricing-price">$249.00 MX/mes</h3>
+                        <div class="box-pricing-time">¡ Oferta !</div><br>
+                        <div class="divider"></div>
+                        <div class="box-pricing-divider">
+                            <span><strong>Bacico</strong></span>
+                        </div>
+                        <h6 class="box-pricing-price">Para empezar.</h6>
+                        <ul class="box-pricing-list">
+                            <li class="active">Dominio Gratuito*</li>
+                            <li class="active">Adaptación Móvil*</li>
+                            <li class="active">Optimización SEO*</li>
+                            <li class="active">Seguridad SSL*</li>
+                            <li class="active">Formulario de Contacto*</li>
+                            <li class="active">Chat Integración con WhatsApp*</li>
+                            <li class="active">Apoyo técnico remoto*</li>
+                            <li>1 Ventana</li>
+                            <li>1 Cuenta de email</li>
+                            <li>1 GB de almacenamiento SSD</li>
+                            <li>Hasta 10 imágenes</li>
+                        </ul>
+                        <br>
+                        <div class="divider"></div>
+                        <p class="">*Tiempo de entrega: 4 a 6 días.</p>
+                        <p class="">**Facturando anualmente</p>
+                    </div>
+                    <div class="box-pricing-button"><a class="button button-lg button-block button-gray-4"
+                            href="/contacto">Contratar</a></div>
                 </div>
-                <div class="box-pricing-button"><a class="button button-lg button-block button-gray-4" href="#">Get started</a></div>
-              </div>
             </div>
             <div class="col-md-6 col-lg-5 col-xl-4">
-              <div class="box-pricing box-pricing-black box-pricing-popular">
+            <div class="box-pricing box-pricing-black box-pricing-popular">
                 <div class="box-pricing-body">
-                  <h5 class="box-pricing-title">Optimal</h5>
-                  <h3 class="box-pricing-price">$800.00</h3>
-                  <div class="box-pricing-time">starting at</div>
-                  <div class="box-pricing-divider">
-                    <div class="divider"></div><span>Optimal</span>
-                  </div>
-                  <ul class="box-pricing-list">
-                    <li class="active">Concept development</li>
-                    <li class="active">UI design</li>
-                    <li class="active">Configuration management</li>
-                    <li class="active">Software quality assurance</li>
-                    <li>App integration</li>
-                  </ul>
+                        <h5 class="box-pricing-title">EMPRENDEDOR <br> PÁGINA BASICA</h5>
+                        <div class="divider"></div>
+                        <h3 class="box-pricing-price">$299.00 MX/mes</h3>
+                        <div class="box-pricing-time">¡ Oferta !</div><br>
+                        <div class="divider"></div>
+                        <div class="box-pricing-divider">
+                            <span><strong>Popular</strong></span>
+                        </div>
+                        <h6 class="box-pricing-price">Ingresa a Nuevos Mercados.</h6>
+                        <ul class="box-pricing-list">
+                            <li class="active">Dominio Gratuito*</li>
+                            <li class="active">Adaptación Móvil*</li>
+                            <li class="active">Optimización SEO*</li>
+                            <li class="active">Seguridad SSL*</li>
+                            <li class="active">Formulario de Contacto*</li>
+                            <li class="active">Chat Integración con WhatsApp*</li>
+                            <li class="active">Apoyo técnico remoto*</li>
+                            <li class="active">Enlace a Redes Sociales*</li>
+                            <li class="active">Mapa de Google Maps*</li>
+                            <li class="active">Estrategia de Posicionamiento*</li>
+                            <li>Hasta 3 Ventana</li>
+                            <li>3 Cuenta de email</li>
+                            <li>5 GB de almacenamiento SSD</li>
+                            <li>Hasta 15 imágenes</li>
+                        </ul>
+                        <br>
+                        <div class="divider"></div>
+                        <p class="">*Tiempo de entrega: 6 a 8 días.</p>
+                        <p class="">**Facturando anualmente</p>
+                    </div>
+                    <div class="box-pricing-button"><a class="button button-lg button-block button-primary"
+                            href="/contacto">Contratar</a></div>
+                            <div class="box-pricing-badge">popular</div>
                 </div>
-                <div class="box-pricing-button"><a class="button button-lg button-block button-primary" href="#">Get started</a></div>
-                <div class="box-pricing-badge">popular</div>
-              </div>
             </div>
             <div class="col-md-6 col-lg-5 col-xl-4">
-              <div class="box-pricing box-pricing-black">
-                <div class="box-pricing-body">
-                  <h5 class="box-pricing-title">Ultimate</h5>
-                  <h3 class="box-pricing-price">$1200.00</h3>
-                  <div class="box-pricing-time">starting at</div>
-                  <div class="box-pricing-divider">
-                    <div class="divider"></div><span>Ultimate</span>
-                  </div>
-                  <ul class="box-pricing-list">
-                    <li class="active">Concept development</li>
-                    <li class="active">UI design</li>
-                    <li class="active">Configuration management</li>
-                    <li class="active">Software quality assurance</li>
-                    <li class="active">App integration</li>
-                  </ul>
+                <div class="box-pricing box-pricing-black">
+                    <div class="box-pricing-body">
+                        <h5 class="box-pricing-title">NEGOCIOS <br>PÁGINA PROFESIONAL</h5>
+                        <div class="divider"></div>
+                        <h3 class="box-pricing-price">$429.00 MX/mes</h3>
+                        <div class="box-pricing-time">¡ Oferta !</div><br>
+                        <div class="divider"></div>
+                        <div class="box-pricing-divider">
+                            <span><strong>Ultra</strong></span>
+                        </div>
+                        <h6 class="box-pricing-price">Sube al Siguiente Nivel.</h6>
+                        <ul class="box-pricing-list">
+                            <li class="active">Dominio Gratuito*</li>
+                            <li class="active">Adaptación Móvil*</li>
+                            <li class="active">Optimización SEO*</li>
+                            <li class="active">Seguridad SSL*</li>
+                            <li class="active">Formulario de Contacto*</li>
+                            <li class="active">Chat Integración con WhatsApp*</li>
+                            <li class="active">Apoyo técnico remoto*</li>
+                            <li class="active">Enlace a Redes Sociales*</li>
+                            <li class="active">Mapa de Google Maps*</li>
+                            <li class="active">Estrategia de Posicionamiento*</li>
+                            <li>Hasta 6 Ventana</li>
+                            <li>10 Cuenta de email</li>
+                            <li>12 GB de almacenamiento SSD</li>
+                            <li>Hasta 30 imágenes</li>
+                            <li>Diseño de Logo básico</li>
+                            <li>Chatbox IA contestación al instante</li>
+                        </ul>
+                        <br>
+                        <div class="divider"></div>
+                        <p class="">*Tiempo de entrega: 8 a 10 días.</p>
+                        <p class="">**Facturando anualmente</p>
+                    </div>
+                    <div class="box-pricing-button"><a class="button button-lg button-block button-gray-4"
+                            href="/contacto">Contratar</a></div>
                 </div>
-                <div class="box-pricing-button"><a class="button button-lg button-block button-gray-4" href="#">Get started</a></div>
-              </div>
             </div>
-          </div>
         </div>
-      </section>
-    
+    </div>
+</section>
+
 @endsection
 
 @section('titulo')
-    Inicio
+Inicio
 @endsection
